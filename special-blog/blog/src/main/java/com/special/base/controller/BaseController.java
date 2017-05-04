12a065dd0f3fc45cfe5e9a70a7f45852c70e51c7
张兴盛 <zhangxingsheng@hzhc-tech.com>
2017-05-04 15:35:06 +0800

    提交工程

---
+++ special-blog/blog/src/main/java/com/special/base/controller/BaseController.java
@@ -0,0 +1,35 @@
+/*
+ * Project: blog
+ * 
+ * File Created at 2017年5月4日
+ * 
+ * Copyright 2016 CMCC Corporation Limited.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * ZYHY Company. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license.
+ */
+package com.special.base.controller;
+
+/**
+ * @Type BaseController.java
+ * @Desc 
+ * @author Zxs
+ * @date 2017年5月4日 下午3:31:12
+ * @version 
+ */
+public class BaseController {
+
+}
+
+
+/**
+ * Revision history
+ * -------------------------------------------------------------------------
+ * 
+ * Date Author Note
+ * -------------------------------------------------------------------------
+ * 2017年5月4日 Zxs create
+ */
