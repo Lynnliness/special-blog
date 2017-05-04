1d8f402b1da6034549026bafd53e7bce255a5512
Lynnliness <738353463@qq.com>
2017-05-04 16:01:23 +0800

    larry is a big sb

--- special-blog/blog/src/main/java/com/special/base/controller/BaseController.java
+++ special-blog/blog/src/main/java/com/special/base/controller/BaseController.java
@@ -22,7 +22,7 @@ package com.special.base.controller;
  */
 public class BaseController {
     public static void main(String[] args) {
-        System.out.println("larry is a sb");
+        System.out.println("larry is a big sb");
     }
 } 
 
